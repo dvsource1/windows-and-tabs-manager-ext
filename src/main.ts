2addffbe0498e35f20d9dc117d1907870d6994ba
dvsource1 <virajkaush@gmail.com>
2022-12-13 20:25:22 +0530

    tabs API

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,36 @@
+import * as _ from "lodash";
 import "./style.css";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <h2>Window & Tabs Manager</h2>
-`;
+const getAllMonitors = async () => {
+  const monitors = await chrome.system.display.getInfo();
+  console.log(monitors);
+  return monitors;
+};
+const getAllWindows = async () => {
+  const windows = await chrome.windows.getAll({ populate: true });
+  console.log(windows);
+  return windows;
+};
+
+const actions = [
+  {
+    id: "getAllMonitors",
+    name: "getAllMonitors",
+    callback: getAllMonitors,
+  },
+  {
+    id: "getAllWindows",
+    name: "getAllWindows",
+    callback: getAllWindows,
+  },
+];
+
+const container = document.getElementById("app");
+
+_.forEach(actions, (action) => {
+  const button = document.createElement("button");
+  button.append(document.createTextNode(action.name));
+  button.setAttribute("id", action.id);
+  button.addEventListener("click", action.callback);
+  container.append(button);
+});
